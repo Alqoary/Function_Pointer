12a40dd336cbacab58607e441a5dd0f2d56ffdb2
apo apo <alquaaryabdalrhman@gmail.com>
2021-09-13 20:51:08 +0300

    add files

---
+++ include/funP.h
@@ -0,0 +1,12 @@
+#ifndef __FUNP__
+#define __FUNP__
+
+
+float sum(float x, float y);
+float sub(float x, float y);
+float div(float x, float y);
+float mul(float x, float y);
+
+void pri();
+
+#endif
---
+++ src/funP.c
@@ -0,0 +1,12 @@
+#include "funP.h"
+#include <stdio.h>
+
+float sum(float x, float y){ return x + y; }
+float sub(float x, float y){ return x - y; }
+float mul(float x, float y){ return x * y; }
+float div(float x, float y){ return x / y; }
+
+
+void pri(){
+	printf("pri");
+}
